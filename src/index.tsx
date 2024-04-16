871eb1685697c7d65e3d810b20471ad2eadfeaf5
Jinnan Zhang <vannizhang@gmail.com>
2024-04-16 15:39:05 -0700

    only import Portal from @arcgis/core

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import OAuthInfo from '@arcgis/core/identity/OAuthInfo';
-import esriId from '@arcgis/core/identity/IdentityManager';
+// import OAuthInfo from '@arcgis/core/identity/OAuthInfo';
+// import esriId from '@arcgis/core/identity/IdentityManager';
 import Portal from '@arcgis/core/portal/Portal';
-import Credential from '@arcgis/core/identity/Credential';
-import PortalUser from '@arcgis/core/portal/PortalUser';
+// import Credential from '@arcgis/core/identity/Credential';
+// import PortalUser from '@arcgis/core/portal/PortalUser';
 
 console.log(
-    OAuthInfo,
-    esriId,
+    // OAuthInfo,
+    // esriId,
     Portal,
-    Credential,
-    PortalUser
+    // Credential,
+    // PortalUser
 )
